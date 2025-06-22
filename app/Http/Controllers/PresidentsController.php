6e5cf69f90dc53a4ab3b9ab030fd27e3495d2d28
bill <bill@hey.com>
2025-06-22 15:51:54 -0700

    added BigQuery package and simple call to table

--- app/Http/Controllers/PresidentsController.php
+++ app/Http/Controllers/PresidentsController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Google\Cloud\BigQuery\BigQueryClient;
+
 class PresidentsController extends Controller
 {
     public function index(): string
     {
-        return "George Washington";
+        $bigquery = new BigQueryClient([
+            'projectId' => 'rise-take-home-463523'
+        ]);
+
+        $query = $bigquery->query('SELECT * FROM us_presidents.presidents ORDER BY name');
+        $results = $bigquery->runQuery($query);
+
+        foreach ($results as $row) {
+            return response()->json($row);
+        }
+
+        return response()->json(['message' => 'No data found'], 404);
     }
 }
