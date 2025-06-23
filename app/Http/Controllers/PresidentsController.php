7cae7d5063efc0142d457b966d41d16819bd8261
bill <bill@hey.com>
2025-06-23 10:04:32 -0700

    fixed local and modified simple controller

--- app/Http/Controllers/PresidentsController.php
+++ app/Http/Controllers/PresidentsController.php
@@ -6,19 +6,23 @@ use Google\Cloud\BigQuery\BigQueryClient;
 
 class PresidentsController extends Controller
 {
-    public function index(): string
+    public function index()
     {
-        $bigquery = new BigQueryClient([
-            'projectId' => 'rise-take-home-463523'
-        ]);
+        try {
+            $bigquery = new BigQueryClient(['projectId' => 'rise-take-home-463523']);
 
-        $query = $bigquery->query('SELECT * FROM us_presidents.presidents ORDER BY name');
-        $results = $bigquery->runQuery($query);
+            $query = $bigquery->query('SELECT * FROM us_presidents.presidents WHERE No_ = 1');
+            $results = $bigquery->runQuery($query);
 
-        foreach ($results as $row) {
-            return response()->json($row);
+            foreach ($results as $row) {
+                return response()->json($row);
+            }
+            return response()->json(['message' => 'No results found'], 404);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'code' => $e->getCode()
+            ], 500);
         }
-
-        return response()->json(['message' => 'No data found'], 404);
     }
 }
